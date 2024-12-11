0d668070a20b087510fb94f768bbc4f97b4172d5
teiron3 <36124310+teiron3@users.noreply.github.com>
2024-12-12 07:09:55 +0900

    Add files via upload

---
+++ parts/csheader.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
